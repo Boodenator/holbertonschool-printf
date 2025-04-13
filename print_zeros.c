5e14aeb1487ad44e6042bc26451d3d7e2fdd3a04
Boodenator <boode1000@gmail.com>
2025-04-13 15:03:05 +0000

    task 12

---
+++ print_zeros.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_zeros: print and handles 0 flags
+ * @args: va_list
+ * @width: minimum width of output
+ *
+ *@pad: padding charachter ('0' for zero flag)
+ *
+ * return: number of printed charachters
+ */
+
+int print_zeros(va_list args, int width, char pad)
+{
+	 int num = va_arg(args, int), count = 0, len = 0, negative = 0;
+
