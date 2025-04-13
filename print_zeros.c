e297bd449235263364471314a5c6443dda955ac4
Boodenator <boode1000@gmail.com>
2025-04-13 15:39:55 +0000

    zeros mod

--- print_zeros.c
+++ print_zeros.c
@@ -12,22 +12,35 @@
 
 int print_zeros(va_list args, int width, char pad)
 {
-	 int num = va_arg(args, int), count = 0, len = 0, neg = 0;
+	int num = va_arg(args, int), count = 0, len = 0, neg = 0;
  	char buf[20];
+	int i, n = num;
+	
 	if (num < 0 )
 	{
 		neg = 1;
 		num = -num;
 	}
-	else
+
+	do {
+		buf[len++] = (n % 10) + '0';
+		n /= 10;
+	}
+	while (n > 0);
+
+	if (neg)
 	{
-		num = num;
+		count += write(1, "-", 1);
+	}
+	
+	for (i = len + neg; i < width; i++)
+	{
+		count += write(1, &pad, 1);
 	}
 
-
-
-
-
-
-
-
+	while (len--)
+	{
+		count += write(1, &buf[len], 1);
+	}
+	return (count);
+}
